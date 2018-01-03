88a571d4ff21f55b8971b470c46f2bd510f8872e
Benny Lodman <bennylodman@gmail.com>
2018-01-03 19:59:29 +0200

    Zookeper functions

--- DS_HW2/src/blockchain/server/zoo/ZooKeeperHandler.java
+++ DS_HW2/src/blockchain/server/zoo/ZooKeeperHandler.java
@@ -1,7 +1,10 @@
 package blockchain.server.zoo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
@@ -11,7 +14,7 @@ import org.apache.zookeeper.ZooKeeper;
 public class ZooKeeperHandler implements Watcher {
 	public static String ZK_ADDR = "??";
 	public static int ZK_PORT = 000;
-	
+
 	private static ZooKeeper zk;
 	private static Object mutex;
 	
@@ -25,4 +28,113 @@ public class ZooKeeperHandler implements Watcher {
             mutex.notify();
         }
     }
+
+	/**
+	 * add new block to chain
+	 * - verify that the path is to the end of the chain and that it is the smallest son added
+	 *
+	 *
+	 * @param path - path to the last znode in the block chain.
+	 * @param data - json string of Block Header (Server Id, Block id).
+	 * @param depth - current block chain length
+	 *
+	 * @return the actual path of the created node
+	 */
+    public String addBlockToBlockChain(String path, String data, int depth)throws KeeperException, InterruptedException
+	{
+		/*If has node in current path -> the chain is already longer*/
+		if(ZookeeperUtils.hasNextBlock(zk,path))
+		{
+			return null;
+		}
+
+		/*Add the new Znode to chain block*/
+		String znodePath = ZookeeperUtils.addPersistentSequentialZNode(zk, path.concat("/" + Integer.toString(depth)), data);
+
+		/*Get all the Znodes sones*/
+		String smallestZnodePath = ZookeeperUtils.returnSmallestSonOfZnode(zk, path);
+
+		/*Check if the node added was the smallest one*/
+		if (znodePath.equals(smallestZnodePath))
+		{
+			return znodePath;
+		}
+		/*Was not the smalles -> remove the son*/
+		ZookeeperUtils.removeZNode(zk, znodePath);
+		return null;
+	}
+
+	/**
+	 * add new server zNode
+	 * - create EPHEMERAL znode with servers name in "Server" subTree
+	 * - this way other servers can now is some server still exists.
+	 *
+	 *
+	 * @param serversName - server name
+	 */
+	public void addServer(String serversName)throws KeeperException, InterruptedException
+	{
+			String path = "/Servers";
+			String serversPath = path.concat("/" + serversName);
+			if (ZookeeperUtils.getAllChildrens(zk, path).contains(serversPath))
+			{
+				/*Server with same name already in the system - not allowed*/
+				assert(false);
+			}
+
+			ZookeeperUtils.addEephemeralZNode(zk,serversPath,serversName);
+	}
+
+	/**
+	 * the function receive apth to znode in the block chain and return the delta from
+	 * this path to the end of the block chain
+	 *
+	 * @param path - server name
+	 */
+	public String getCahinSuffix(String path, int depth)throws KeeperException, InterruptedException
+	{
+		String currentPath = path;
+		int count=0;
+		while(ZookeeperUtils.hasNextBlock(zk, currentPath))
+		{
+			currentPath = getSmallestZnodeName(ZookeeperUtils.getAllChildrens(zk,currentPath));
+			count ++;
+		}
+
+		currentPath.replaceFirst(path, "");
+		return currentPath;
+	}
+
+	/**
+	 * the function receive a path to znode in the block chain and return in a list all the
+	 * data contained in the next Znode
+	 * (Need it in order to have the missing blocks and the servers created them and the depth)
+	 *
+	 * @param path - server name
+	 */
+	public List<String> getAllTheNextBlocks(String path, int depth)throws KeeperException, InterruptedException
+	{
+		List<String> blockList = new ArrayList<>();
+
+		////Continue here
+
+		return blockList;
+	}
+
+	private String getSmallestZnodeName(List<String> list)
+	{
+		assert(!list.isEmpty());
+		String small = list.get(0);
+		for (String child : list)
+		{
+			if(small.compareTo(child) > 0)
+			{
+				small = child;
+			}
+		}
+		return small;
+
+	}
+
+
 }
--- DS_HW2/src/blockchain/server/zoo/ZookeeperUtils.java
+++ DS_HW2/src/blockchain/server/zoo/ZookeeperUtils.java
@@ -1,9 +1,7 @@
 package blockchain.server.zoo;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-
+import blockchain.server.model.Block;
+import com.google.gson.Gson;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs.Ids;
@@ -12,9 +10,10 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Stat;
 
-import com.google.gson.Gson;
-
-import blockchain.server.model.Block;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ZookeeperUtils {
 	public static String TEMP_NODE_PREFIX = "__";
@@ -37,69 +36,87 @@ public class ZookeeperUtils {
 		return path + String.valueOf(depth);
 	}
 	
-	
 	/**
-	 * check if the znode which specified at 'path' hase child which is a Block (not temporal node)
+	 * check if the znode which specified at 'path' has child
 	 * 
 	 * @param path - path to the znode which we want to check
 	 * 
 	 */
 	public static boolean hasNextBlock(ZooKeeper zk, String path) throws KeeperException, InterruptedException {
 		List<String> childrenList = zk.getChildren(path, false);
-		for (String nodeName : childrenList) {
-			if (!nodeName.startsWith(TEMP_NODE_PREFIX))
-				return true;
-		}
-		return false;
+		return !childrenList.isEmpty();
 	}
-	
-	
+
 	/**
-	 * created persistent znode after the node which specified at 'path' 
+	 * created  znode after the node which specified at 'path'
 	 * 
 	 * @param path - path to the znode which we want to add block after it.
-	 * @param data - json string of Block class.
+	 * @param data - json string of Block class header.
 	 * 
 	 * @return the actual path of the created node
 	 */
-	public static String createBlock(ZooKeeper zk, String path, String data) throws KeeperException, InterruptedException {
+	public static String addPersistentSequentialZNode(ZooKeeper zk, String path, String data) throws KeeperException, InterruptedException {
 		byte[] dataAsBytes = data.getBytes(StandardCharsets.UTF_8);
-		List<ACL> acl = Arrays.asList(new ACL[]{new ACL(Perms.ALL, Ids.ANYONE_ID_UNSAFE)});
-		return zk.create(path, dataAsBytes, acl, CreateMode.PERSISTENT);
+
+		return zk.create(path, dataAsBytes, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
 	}
-	
-	
+
 	/**
-	 * created persistent znode after the node which specified at 'path' 
-	 * 
-	 * @param path - path to the znode which we want to add block after it.
-	 * @param data - Block object which we want to set as data of the new node.
-	 * 
+	 * created  znode after the node which specified at 'path'
+	 *
+	 * @param path - path to the znode which we want to remove
+	 *
+	 */
+	public static void removeZNode(ZooKeeper zk, String path) throws KeeperException, InterruptedException {
+		zk.delete(path, zk.exists(path,true).getVersion());
+	}
+
+	/**
+	 * return the name of the smallest son of the node in the path
+	 *
+	 * @param path - path to the znode that we want to find his smallest son.
+	 *
 	 * @return the actual path of the created node
 	 */
-	public static String createBlock(ZooKeeper zk, String path, Block data) throws KeeperException, InterruptedException {
-		String dataAsString = gson.toJson(data);
-		return createBlock(zk, path, dataAsString);
+	public static String returnSmallestSonOfZnode(ZooKeeper zk, String path) throws KeeperException, InterruptedException {
+		List<String> childrenList = zk.getChildren(path, false);
+		String smallest = childrenList.get(0);
+		for (String child : childrenList)
+		{
+			if(smallest.compareTo(child) > 0)
+			{
+				smallest = child;
+			}
+		}
+		return smallest;
 	}
-	
-	
+
 	/**
-	 * created persistent znode after the node which specified at 'path', the new node name will with TEMP_NODE_PREFIX
-	 * and then continued with generated sequential number.
-	 * 
-	 * @param path - path to the znode which we want to add temp node after it.
-	 * 
+	 * return znode od type EPHEMERAL
+	 * @param path - path to the znode that we want to find his smallest son.
+	 *
 	 * @return the actual path of the created node
 	 */
-	public static String createTemporalNode(ZooKeeper zk, String path) throws KeeperException, InterruptedException {
-		List<ACL> acl = Arrays.asList(new ACL[]{new ACL(Perms.ALL, Ids.ANYONE_ID_UNSAFE)});
-		return zk.create(path, new byte[0], acl, CreateMode.PERSISTENT_SEQUENTIAL);
+	public static String addEephemeralZNode(ZooKeeper zk, String path, String data) throws KeeperException, InterruptedException {
+		byte[] dataAsBytes = data.getBytes(StandardCharsets.UTF_8);
+		return zk.create(path, dataAsBytes, Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 	}
-	
+
+	/**
+	 * get sons of znode
+	 * * @param path - path to the znode that we want to find his sons.
+	 *
+	 * @return list of sons (if path does not exist will return empty list)
+	 */
+
 	public static List<String> getAllChildrens(ZooKeeper zk, String path) throws KeeperException, InterruptedException {
-		return zk.getChildren(path, false);
+		if (null != zk.exists(path, null))
+		{
+			return zk.getChildren(path, false);
+		}
+		return new ArrayList<>();
 	}
-	
+
 	public static int getNodeVersion(ZooKeeper zk, String path) throws KeeperException, InterruptedException {
 		Stat stat = new Stat();
 		zk.getData(path, false, stat);
@@ -107,15 +124,7 @@ public class ZookeeperUtils {
 	}
 	
 	
-	/**
-	 * @param path - path to the znode which we want to delete
-	 * @param version - the version of the znode (can achieved by calling to getNodeVersion(..))
-	 */
-	public static void deleteNode(ZooKeeper zk, String path, int version) throws InterruptedException, KeeperException {
-		zk.delete(path, version);
-	}
-	
-	
+
 	/**
 	 * @param path - path to the znode which we want to read it's data
 	 * @param cls -  class of the object type of the znode data. (Block\Ship\Container\Item).class
@@ -129,13 +138,4 @@ public class ZookeeperUtils {
 		return gson.fromJson(dataAsString, cls);
 	} 
 
-	
-	public static void waitForNodeDeletion(ZooKeeper zk, String path, Object mutex) throws KeeperException, InterruptedException { //this function return only when the node was deleted.
-		synchronized (mutex) {
-			Stat stat = zk.exists(path, true);
-	        if (stat != null) {
-	            mutex.wait();
-	        } 
-		}
-	} 
 }
